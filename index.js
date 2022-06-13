560a8731d84426e86d7058bf30f43862baf69b2c
annaclouds <napha@ymail.com>
2022-06-13 00:58:25 -0700

    changed the font and color of the dog descriptions. changed the color of the header.

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ let header = document.querySelector('#page-header');        // header is defined
 
 // 2. Modify the styling of an element. 
 header.style.textAlign = "left";                            // changing how the header text is aligned.
+header.style.color = 'brown';                               // changed the color of the header.
 
 // 3. Select multiple elements.
 let dogImages = document.querySelectorAll('.dog-image');    // selecting all the dog images; defined as an array of DOM elements.
@@ -27,3 +28,12 @@ for (let i = 0; i < dogNames.length; i++) {                // using a loop to ch
 // changing the color of the footer text.
 let footerText = document.querySelector('.footer');
 footerText.style.color = 'blue';
+
+// changing the font and color of the dog descriptions.
+let dogDescription = document.querySelectorAll('.dog-description'); // selecting all the dog descriptions.
+for (let i = 0; i < dogDescription.length; i++) { 
+    dogDescription[i].style.fontFamily = 'cursive';                 // changed the dog descriptions font.
+    dogDescription[0].style.color = 'green';                        // changed the first description color to green.
+    dogDescription[1].style.color = 'purple';                       // changed the second description color to purple.
+    dogDescription[2].style.color = 'skyblue';                      // changed the third description color to skyblue.
+};
